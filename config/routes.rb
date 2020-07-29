d14acb3862477edae0386626c9477872093becaa
Aaron Landeros <guiza92@hotmail.com>
2020-07-28 19:23:58 -0500

    Added consultation routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
     end
     resources :patient_records, only: [:new, :create, :edit, :update]
     resources :coach_records, only: [:new, :create, :edit, :update]
+    resources :consultations
   end
   # Plans for users
   resources :plans, only: [:index, :show]
